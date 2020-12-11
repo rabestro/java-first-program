3b879922cd3ae96c04251980fa14e21c36df119f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-11 19:02:03 +0200

    Create a Constructor to Accept Values

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,8 +1,14 @@
 package com.h2;
 
 public class MortgageCalculator {
-    private long loanAmount;
-    private int termInYears;
-    private float annualRate;
+    private final long loanAmount;
+    private final int termInYears;
+    private final float annualRate;
     private double monthlyPayment;
+
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
 }
