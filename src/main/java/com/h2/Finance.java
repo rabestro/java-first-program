a68bc51773faeacf4fcd15fb14260991b28a9e12
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-11 19:50:03 +0200

    Create a New Method validateCommandArguments()

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -3,11 +3,15 @@ package com.h2;
 import java.util.Map;
 
 public class Finance {
-    public final static String BEST_LOAN_RATES = "bestLoanRates";
-    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
-    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
-    public final static Map<String, String> commandsToUsage = Map.of(
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
+    public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+
+    private static boolean validateCommandArguments(String[] args) {
+        return false;
+    }
 }
