e01310f33aa49b27bbe98d239a6ca0a8c8f46d71
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-11 19:01:06 +0200

    Create Members to Store Required Inputs

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,4 +1,8 @@
 package com.h2;
 
 public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
 }
