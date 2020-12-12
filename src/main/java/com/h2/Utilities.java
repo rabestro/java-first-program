70f7ad8de64d4e173ba0211a50fd1997e0bb81ca
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-12 10:33:57 +0200

    Implement getLongValue()

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -2,6 +2,12 @@ package com.h2;
 
 public class Utilities {
     public static long getLongValue(final String in) {
-        return 0;
+        var out = Long.MIN_VALUE;
+        try {
+            out = Long.parseLong(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
+        }
+        return out;
     }
 }
